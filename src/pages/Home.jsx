79fa3d48fd48abe10b7837bebc8742d6f5b85f8b
Iwmvictor <isingizwemunezerovictor5@gmail.com>
2024-12-26 12:01:59 +0200

    SEO

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { HelmetProvider, Helmet } from "react-helmet-async";
+
 import "./../style/home.scss";
 import Hero from "./../homepage/Hero";
 import Continue from "./../homepage/Continue";
@@ -11,15 +13,90 @@ import Film from "./../homepage/Film";
 function Homepage() {
   return (
     <>
-      <div className="homepage">
-        
-        <Hero />
-        {/* <Continue />
+      <HelmetProvider>
+        <Helmet>
+          <title>
+            Walluxe | Endless Entertainment with Kinyarwanda Translated Movies
+          </title>
+          <meta
+            name="description"
+            content="Walluxe is your ultimate destination for Kinyarwanda translated movies. Experience endless entertainment tailored for Rwandan audiences and beyond."
+          />
+          <meta
+            name="keywords"
+            content="Kinyarwanda movies, Filime zisobanuye, Rwandan entertainment, Walluxe, translated movies, agasobanuye, streaming platform Rwanda, movie streaming in Rwanda, Kinyarwanda translated films, Rwandan cinema, African movies, Walluxe platform, endless entertainment, film streaming Rwanda"
+          />
+        </Helmet>
+
+        <Helmet>
+          <meta
+            property="og:title"
+            content="Walluxe - Endless Entertainment with Kinyarwanda Movies"
+          />
+          <meta
+            property="og:description"
+            content="Stream Kinyarwanda translated movies on Walluxe. Enjoy a wide selection of films and exclusive entertainment tailored for Rwandans."
+          />
+          <meta property="og:image" content="/image/brand/social.jpg" />
+          <meta property="og:url" content="https://walluxe.vercel.app" />
+          <meta property="og:type" content="website" />
+        </Helmet>
+
+        <Helmet>
+          <link rel="canonical" href="https://walluxe.vercel.app" />
+        </Helmet>
+
+        <Helmet>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-JVLLYDXT92"
+          ></script>
+          <script>
+            {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-JVLLYDXT92');`}
+          </script>
+        </Helmet>
+
+        <Helmet>
+          <script type="application/ld+json">
+            {`
+            {
+              "@context": "https://schema.org",
+              "@type": "LocalBusiness",
+              "name": "Meyvn Agency",
+              "url": "https://meyvn.vercel.app",
+              "logo": "https://meyvn.vercel.app/images/brand/logo.svg",
+              "description": "Meyvn Agency provides software development, graphic design, digital marketing, and IT services in Rwanda.",
+              "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "Main Street",
+                "addressLocality": "Musanze",
+                "addressRegion": "Northern Province",
+                "postalCode": "00000",
+                "addressCountry": "Rwanda"
+              },
+              "geo": {
+                "@type": "GeoCoordinates",
+                "latitude": -1.5000,
+                "longitude": 29.6333
+              },
+              "telephone": "+250781996271",
+              "openingHours": "Mo-Su 08:00-20:00"
+            }
+            `}
+          </script>
+        </Helmet>
+        <div className="homepage">
+          <Hero />
+          {/* <Continue />
         <Latest />
         <Serie />
         <Movie /> */}
-        <Film />
-      </div>
+          <Film />
+        </div>
+      </HelmetProvider>
     </>
   );
 }
--- src/pages/Watch.jsx
+++ src/pages/Watch.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { useLocation } from "react-router-dom";
+import { HelmetProvider, Helmet } from "react-helmet-async";
 
 import { GiBleedingEye } from "react-icons/gi";
 import { TbDownload, TbShare } from "react-icons/tb";
@@ -114,177 +115,243 @@ const Watchpage = () => {
   };
 
   return (
-    <div className="watch">
-      <div className="container">
-        <div className="content">
-          <div className="main">
-            {loading ? (
-              <div className="loader">
-                {/* Skeleton Loader for Video */}
-                <ContentLoader
-                  viewBox="0 0 800 420"
-                  height={420}
-                  width="100%"
-                  backgroundColor="RGB(44, 44, 44)"
-                  foregroundColor="RGB(3, 4,3)"
-                >
-                  <rect x="0" y="0" rx="5" ry="5" width="800" height="420" />
-                </ContentLoader>
-                {/* Skeleton Loader for Title */}
-                <ContentLoader
-                  viewBox="0 0 800 60"
-                  height={60}
-                  width="100%"
-                  backgroundColor="#f3f3f3"
-                  foregroundColor="#ecebeb"
-                >
-                  <rect x="0" y="10" rx="5" ry="5" width="300" height="20" />
-                  <rect x="0" y="40" rx="5" ry="5" width="200" height="15" />
-                </ContentLoader>
-                {/* Skeleton Loader for Description */}
-                <ContentLoader
-                  viewBox="0 0 800 150"
-                  height={150}
-                  width="100%"
-                  backgroundColor="#f3f3f3"
-                  foregroundColor="#ecebeb"
-                >
-                  <rect x="0" y="0" rx="5" ry="5" width="800" height="10" />
-                  <rect x="0" y="20" rx="5" ry="5" width="600" height="10" />
-                  <rect x="0" y="40" rx="5" ry="5" width="700" height="10" />
-                  <rect x="0" y="60" rx="5" ry="5" width="500" height="10" />
-                </ContentLoader>
-              </div>
-            ) : (
-              <div className="video">
-                <div className="frame">
-                  <iframe
-                    src={embedLink}
-                    frameBorder="0"
-                    allowFullScreen
-                    title={movie?.title || videoInfo?.file_title}
-                    style={{
-                      width: "100%",
-                      height: "420px",
-                    }}
-                  ></iframe>
-                </div>
-                <h2>{videoInfo?.file_title || movie?.title}</h2>
-                <div className="actions">
-                  <button className="btn" onClick={handleDownload}>
-                    <TbDownload /> Download
-                  </button>
-                  <button className="btn" onClick={handleCopy}>
-                    <TbShare /> Share
-                  </button>
-                  <Toast message="Link copied!" show={showToast} />
-                  <button>
-                    <GiBleedingEye /> {videoInfo?.file_views || "0"} Views
-                  </button>
-                </div>
-                <div className="descr">
-                  <p style={{ textTransform: "uppercase" }}>
-                    {videoInfo?.file_title}
-                  </p>
-                  <i
-                    style={{
-                      fontSize: ".8rem",
-                      fontWeight: "100",
-                      padding: ".6rem",
-                      display: "block",
-                      opacity: ".6",
-                    }}
-                  >
-                    {videoInfo?.tags || "No tags available"}
-                  </i>
-                  <p>
-                    <strong>Duration:</strong>{" "}
-                    {videoInfo?.file_length
-                      ? `${Math.floor(
-                          videoInfo.file_length / 3600
-                        )}h ${Math.floor(
-                          (videoInfo.file_length % 3600) / 60
-                        )}min ${Math.floor(
-                          (videoInfo.file_length % 3600) % 60
-                        )}sec`
-                      : "N/A"}
-                  </p>
-                  <p
-                    style={{
-                      fontSize: ".9rem",
-                      opacity: ".6",
-                      marginTop: ".3rem",
-                    }}
-                  >
-                    • {videoInfo?.file_created || "Unknown"}
-                  </p>
-                </div>
-              </div>
-            )}
-
-            <div className="comment">
-              <h2>Leave a Comment</h2>
-              <form onSubmit={handleCommentSubmit}>
-                <input
-                  type="text"
-                  placeholder="Name"
-                  required
-                  value={commentData.name}
-                  onChange={(e) =>
-                    setCommentData({ ...commentData, name: e.target.value })
-                  }
-                  style={{ margin: "10px 0" }}
-                />
-                <textarea
-                  placeholder="Message"
-                  required
-                  value={commentData.message}
-                  onChange={(e) =>
-                    setCommentData({ ...commentData, message: e.target.value })
-                  }
-                  style={{ width: "100%", height: "100px" }}
-                ></textarea>
-                <button type="submit">Submit</button>
-              </form>
-              <div className="comments-section">
-                <h3>Comments</h3>
-                {comments.map((comment, index) => (
-                  <div key={index} className="comment-item">
-                    <h4>{comment.name}</h4>
-                    <p>{comment.message}</p>
+    <>
+      <HelmetProvider>
+        <div className="watch">
+          <Helmet>
+            <title>
+              {videoInfo?.file_title + " ©  walluxe[meyvn agency]" ||
+                "Watch Movie | Walluxe"}
+            </title>
+            <meta
+              name="description"
+              content={
+                videoInfo?.file_description ||
+                "Watch the latest and trending movies on Walluxe, your ultimate movie streaming platform."
+              }
+            />
+            <meta
+              name="keywords"
+              content="movie, streaming, Walluxe, watch online, movies, agasobanuye, mu kinyarwanda, translator mukinyarwanda, kigali rwanda"
+            />
+          </Helmet>
+          <div className="container">
+            <div className="content">
+              <div className="main">
+                {loading ? (
+                  <div className="loader">
+                    {/* Skeleton Loader for Video */}
+                    <ContentLoader
+                      viewBox="0 0 800 420"
+                      height={420}
+                      width="100%"
+                      backgroundColor="RGB(44, 44, 44)"
+                      foregroundColor="RGB(3, 4,3)"
+                    >
+                      <rect
+                        x="0"
+                        y="0"
+                        rx="5"
+                        ry="5"
+                        width="800"
+                        height="420"
+                      />
+                    </ContentLoader>
+                    {/* Skeleton Loader for Title */}
+                    <ContentLoader
+                      viewBox="0 0 800 60"
+                      height={60}
+                      width="100%"
+                      backgroundColor="#f3f3f3"
+                      foregroundColor="#ecebeb"
+                    >
+                      <rect
+                        x="0"
+                        y="10"
+                        rx="5"
+                        ry="5"
+                        width="300"
+                        height="20"
+                      />
+                      <rect
+                        x="0"
+                        y="40"
+                        rx="5"
+                        ry="5"
+                        width="200"
+                        height="15"
+                      />
+                    </ContentLoader>
+                    {/* Skeleton Loader for Description */}
+                    <ContentLoader
+                      viewBox="0 0 800 150"
+                      height={150}
+                      width="100%"
+                      backgroundColor="#f3f3f3"
+                      foregroundColor="#ecebeb"
+                    >
+                      <rect x="0" y="0" rx="5" ry="5" width="800" height="10" />
+                      <rect
+                        x="0"
+                        y="20"
+                        rx="5"
+                        ry="5"
+                        width="600"
+                        height="10"
+                      />
+                      <rect
+                        x="0"
+                        y="40"
+                        rx="5"
+                        ry="5"
+                        width="700"
+                        height="10"
+                      />
+                      <rect
+                        x="0"
+                        y="60"
+                        rx="5"
+                        ry="5"
+                        width="500"
+                        height="10"
+                      />
+                    </ContentLoader>
                   </div>
-                ))}
-              </div>
-            </div>
-          </div>
+                ) : (
+                  <div className="video">
+                    <div className="frame">
+                      <iframe
+                        src={embedLink}
+                        frameBorder="0"
+                        allowFullScreen
+                        title={movie?.title || videoInfo?.file_title}
+                        style={{
+                          width: "100%",
+                          height: "420px",
+                        }}
+                      ></iframe>
+                    </div>
+                    <h2>{videoInfo?.file_title || movie?.title}</h2>
+                    <div className="actions">
+                      <button className="btn" onClick={handleDownload}>
+                        <TbDownload /> Download
+                      </button>
+                      <button className="btn" onClick={handleCopy}>
+                        <TbShare /> Share
+                      </button>
+                      <Toast message="Link copied!" show={showToast} />
+                      <button>
+                        <GiBleedingEye /> {videoInfo?.file_views || "0"} Views
+                      </button>
+                    </div>
+                    <div className="descr">
+                      <p style={{ textTransform: "uppercase" }}>
+                        {videoInfo?.file_title}
+                      </p>
+                      <i
+                        style={{
+                          fontSize: ".8rem",
+                          fontWeight: "100",
+                          padding: ".6rem",
+                          display: "block",
+                          opacity: ".6",
+                        }}
+                      >
+                        {videoInfo?.tags || "No tags available"}
+                      </i>
+                      <p>
+                        <strong>Duration:</strong>{" "}
+                        {videoInfo?.file_length
+                          ? `${Math.floor(
+                              videoInfo.file_length / 3600
+                            )}h ${Math.floor(
+                              (videoInfo.file_length % 3600) / 60
+                            )}min ${Math.floor(
+                              (videoInfo.file_length % 3600) % 60
+                            )}sec`
+                          : "N/A"}
+                      </p>
+                      <p
+                        style={{
+                          fontSize: ".9rem",
+                          opacity: ".6",
+                          marginTop: ".3rem",
+                        }}
+                      >
+                        • {videoInfo?.file_created || "Unknown"}
+                      </p>
+                    </div>
+                  </div>
+                )}
 
-          <div className="sidebar">
-            <div className="cta">
-              <h2>Don't miss out!</h2>
-              <button>Subscribe Now</button>
+                <div className="comment">
+                  <h2>Leave a Comment</h2>
+                  <form onSubmit={handleCommentSubmit}>
+                    <input
+                      type="text"
+                      placeholder="Name"
+                      required
+                      value={commentData.name}
+                      onChange={(e) =>
+                        setCommentData({ ...commentData, name: e.target.value })
+                      }
+                      style={{ margin: "10px 0" }}
+                    />
+                    <textarea
+                      placeholder="Message"
+                      required
+                      value={commentData.message}
+                      onChange={(e) =>
+                        setCommentData({
+                          ...commentData,
+                          message: e.target.value,
+                        })
+                      }
+                      style={{ width: "100%", height: "100px" }}
+                    ></textarea>
+                    <button type="submit">Submit</button>
+                  </form>
+                  <div className="comments-section">
+                    <h3>Comments</h3>
+                    {comments.map((comment, index) => (
+                      <div key={index} className="comment-item">
+                        <h4>{comment.name}</h4>
+                        <p>{comment.message}</p>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+
+              <div className="sidebar">
+                <div className="cta">
+                  <h2>Don't miss out!</h2>
+                  <button>Subscribe Now</button>
+                </div>
+                <Link to={"https://meyvn.vercel.app/"} className="advert">
+                  <div className="frame">
+                    <iframe
+                      src="https://meyvn.vercel.app/"
+                      frameBorder="0"
+                      allowFullScreen
+                      title="meyvn agency"
+                      style={{
+                        width: "100%",
+                        height: "620px",
+                      }}
+                    ></iframe>
+                  </div>
+                  <div className="name">
+                    <h3>Meyvn Agency</h3>
+                    <p>Your software development partner</p>
+                  </div>
+                </Link>
+              </div>
             </div>
-            <Link to={"https://meyvn.vercel.app/"} className="advert">
-              <div className="frame">
-                <iframe
-                  src="https://meyvn.vercel.app/"
-                  frameBorder="0"
-                  allowFullScreen
-                  title="meyvn agency"
-                  style={{
-                    width: "100%",
-                    height: "620px",
-                  }}
-                ></iframe>
-              </div>
-              <div className="name">
-                <h3>Meyvn Agency</h3>
-                <p>Your software development partner</p>
-              </div>
-            </Link>
           </div>
         </div>
-      </div>
-    </div>
+      </HelmetProvider>
+    </>
   );
 };
 
